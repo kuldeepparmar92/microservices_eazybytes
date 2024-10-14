9080397bbeef0e15fb6f1a5bcf3dfe7616c0ba3a
Kuldeepsinh Parmar <kuldeepparmar92@gmail.com>
2024-10-14 19:09:10 +0530

    Build Accounts, Loans and Cards Microservices

---
+++ section2/cards/src/main/java/com/eazybytes/cards/CardsApplication.java
@@ -0,0 +1,40 @@
+package com.eazybytes.cards;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "EazyBank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Kuldeepsinh Parmar",
+						email = "kuldeepparmar92@gmail.com",
+						url = "http://www.ermin.co.in"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "http://www.ermin.co.in"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Cards Microservices REST API Documentation",
+				url = "http://www.ermin.co.in/swagger-ui.html"
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+
+}
